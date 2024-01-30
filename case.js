3dd93208ce003aa520fc66d97fe75f276b926629
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-30 21:22:55 +0600

    uppercase and lowercase

--- case.js
+++ case.js
@@ -7,3 +7,20 @@ if(subject === book){
 else{
     console.log('fapor baz ekta');
 }
+
+const fileName = 'generel file';
+const look = 'Generel file';
+
+// we can make the whole string into lowecase with this>>>
+console.log(fileName);
+console.log(fileName.toLowerCase());
+// thus we can make it uppercase tooo>>>>>
+console.log(look)
+console.log(look.toUpperCase());
+
+if(fileName === look){
+    console.log('looking good');
+}
+else{
+    console.log('your file is not here');
+}
