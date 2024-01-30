737620db005cddd1b9c8ea3c6003374842ab1133
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-30 21:06:53 +0600

    array and string are different

--- string_array.js
+++ string_array.js
@@ -2,3 +2,8 @@ let numbers = [1, 5, 6, 45, 78, 9];
 console.log(numbers);
 numbers[0] = 12365; // we can change a value through selecting the index of an array.
 console.log(numbers);
+
+const name = 'MD.Rony Sorker';
+console.log(name);
+console.log(name.length);
+console.log(name[3]); // we can find the index of an string with this method.
