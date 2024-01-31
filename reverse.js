0f736bc74ae7eb7510a97db3837e34b4903533b6
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-31 14:49:59 +0600

    reverse

---
+++ reverse.js
@@ -0,0 +1,8 @@
+let sentence = 'i am good boy';
+let reverse = ''; 
+for(let letter of sentence){
+    // console.log(letter);
+    reverse = letter + reverse;
+}
+console.log(reverse);
+// we can reverse a sentence with this method.
