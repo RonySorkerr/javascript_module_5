794fc17f1976a26ac9c6bff416113ab9134565a1
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-31 15:07:42 +0600

    dodne

--- reverse.js
+++ reverse.js
@@ -1,8 +1,13 @@
 let sentence = 'i am good boy';
-let reverse = ''; 
-for(let letter of sentence){
+let reverse = '';
+for (let letter of sentence) {
     // console.log(letter);
     reverse = letter + reverse;
 }
 console.log(reverse);
 // we can reverse a sentence with this method.
+
+const bakko = 'he is good in math';
+for (i = 0; i < bakko.length; i++) {
+    console.log(i);
+}
