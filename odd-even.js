e759b4dc68e4a4a47211cc3d75026f018838be66
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-01 08:12:57 +0600

    divided by 5

--- odd-even.js
+++ odd-even.js
@@ -1,3 +1,13 @@
 for(let i=0; i<20; i+=2){
     console.log(i);
 }
+
+for(let x = 1; x<10; x+=2){
+    console.log(x)
+}
+
+for(n = 1; n<= 30; n++){
+    if(n % 5 === 0){
+        console.log(n);
+    }
+}
