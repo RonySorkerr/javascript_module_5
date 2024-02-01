e2106a0696fcbe4d8aa755931f5dde1729d0f043
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-01 09:00:10 +0600

    total of odd number

---
+++ break.js
@@ -0,0 +1,13 @@
+let total = 0;
+for(let i = 81; i <= 181; i++){
+    if(i % 2 === 0){
+        // console.log(i);
+        total = total + i;
+    }
+}
+    console.log('the odds sum is : ' , total)
+
+    // console.log(i);
+    // total = total + i;
+
+console.log('the total is : ' , total);
