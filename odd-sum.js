1e77e4a79b357fc8321eedd614d32eab87c83e2b
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-01 10:05:58 +0600

    even numbers sum

--- odd-sum.js
+++ odd-sum.js
@@ -1,8 +1,18 @@
 total = 0;
 for (let i = 91; i <= 129; i++) {
     if (i % 2 === 1) {
-        console.log(i);
+        // console.log(i);
     }
     total += i;
 }
 console.log('your odd numbers total is: ' , total);
+
+
+total2 = 0;
+for(let n = 51; n <= 85; n++){
+    if(n % 2 === 0){
+        // console.log(n);
+    }
+    total2 += n;
+}
+console.log('your even numbers total is: ' , total2);
