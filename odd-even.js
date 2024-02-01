e452b89f4b5fbeb4276e2a769e45964dbdeb4d94
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-01 08:23:17 +0600

    odd even

--- odd-even.js
+++ odd-even.js
@@ -1,13 +1,19 @@
-for(let i=0; i<20; i+=2){
+for (let i = 0; i < 20; i += 2) {
     console.log(i);
 }
 
-for(let x = 1; x<10; x+=2){
+for (let x = 1; x < 10; x += 2) {
     console.log(x)
 }
 
-for(n = 1; n<= 30; n++){
-    if(n % 5 === 0){
+for (n = 1; n <= 30; n++) {
+    if (n % 5 === 0) {
         console.log(n);
+    }
+}
+
+for(m = 0; m<= 50; m++){
+    if(m%3===0 || m%5===0){
+        console.log('your ams are : ' , m);
     }
 }
