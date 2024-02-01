a7d2f560a70c3f927a0c8af900936f989ce9cd92
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-01 08:08:59 +0600

    odd even.js

---
+++ sum.js
@@ -0,0 +1,15 @@
+let sum = 0;
+for(let i = 81; i <= 131; i++){
+    sum =  sum + i;
+    // console.log(i);
+}
+// console.log(sum);
+
+let num = 120;
+let Sum = 0;
+while(num <= 125){
+    console.log(num);
+    Sum = Sum + num;
+    console.log('your total sum is: ' , Sum);
+    num++;
+}
